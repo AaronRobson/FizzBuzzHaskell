87bf656959ba4453d020a471544cf139c3ec989a
Aaron Robson <ARobsonSE@hotmail.co.uk>
2016-06-24 02:05:30 +0100

    With the type annotation for "fizzbuzz".

--- FizzBuzz.hs
+++ FizzBuzz.hs
@@ -2,6 +2,7 @@ module FizzBuzz where
 
 import Control.Monad (mapM_)
 
+fizzbuzz :: (Show a, Integral a) => a -> String
 fizzbuzz n = if null s then (show n) else s
   where
     s = (if (n `mod` 3) == 0 then "Fizz" else "") ++ (if (n `mod` 5) == 0 then "Buzz" else "")
